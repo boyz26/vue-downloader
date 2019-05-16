57495866c610085a610652a1b6570daf8afef0a9
boyz26 <boyz26@gmail.com>
2019-05-16 17:06:09 +0800

    misc error
    export 'BaseDownloader' was not found in './components/BaseDownloader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Downloader from './components/Downloader';
-import { BaseDownloader } from './components/BaseDownloader';
+import BaseDownloader from './components/BaseDownloader';
 import DownloaderItem from './components/DownloaderItem';
-import { BaseDownloaderItem } from './components/BaseDownloaderItem';
+import BaseDownloaderItem from './components/BaseDownloaderItem';
 
 export default Downloader;
 export {
